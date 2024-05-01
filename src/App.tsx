e00049182258b2a7bad07b001902b771852dd9d2
Z4nR <ari.comunity1@gmail.com>
2024-05-01 22:31:16 +0700

    chore: update form for info data swagger

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { Separator } from './components/ui/separator';
 import { SwaggerForm } from './pages/form-pages';
-import './styles/index.css'
+import './styles/index.css';
 
 function App() {
-
   return (
-    <>
-      <SwaggerForm/>
-    </>
+    <div className="w-full">
+      <div className="max-w-xl my-0 mx-auto py-2">
+        <h3 className='mt-8 scroll-m-20 text-2xl font-semibold tracking-tight"'>
+          Generate Your Swagger Spec
+        </h3>
+        <Separator className="my-4" />
+        <SwaggerForm />
+      </div>
+    </div>
   );
 }
 
--- src/pages/form-pages.tsx
+++ src/pages/form-pages.tsx
@@ -1,8 +1,8 @@
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button';
 import {
   Form,
   FormControl,
@@ -11,60 +11,159 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
 
 const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
+  openapi: z.string({ required_error: 'OpenAPI is required' }),
+  title: z
+    .string()
+    .min(10, {
+      message: 'Title must be at least 10 characters.',
+    })
+    .max(20, {
+      message: 'Title maximal 20 characters.',
+    }),
+  description: z.string().max(255, {
+    message: 'Description maximal 255 characters.',
   }),
-})
+  version: z.string().regex(/^\d+\.\d+\.\d+$/, {
+    message: 'Version must be format like: x.x.x',
+  }),
+  licenseName: z.string({ required_error: 'License Name is required' }),
+  licenseUrl: z.string().url({ message: 'License URL is required' }),
+});
 
 export function SwaggerForm() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      openapi: '3.0.3',
+      title: '',
+      description: '',
+      version: '0.0.0',
+      licenseName: '',
+      licenseUrl: '',
     },
-  })
- 
+  });
+
   function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values)
+    console.log(values);
   }
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <>
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-            </>
-          )}
-        />
-        <Button type="submit">Submit</Button>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="space-y-2">
+          <h4 className="scroll-m-20 text-lg font-semibold tracking-tight">
+            Info
+          </h4>
+          <div className="space-y-2">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Please insert your swagger title"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    This is your swagger file title.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Tell us a little bit about your API"
+                      className=" resize-none"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    This is your swagger description.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="version"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Version</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Please insert your API version"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    This is your API versioning.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex gap-2">
+              <FormField
+                control={form.control}
+                name="licenseName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>OpenAPI License</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Please insert your OpenAPI License Name"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      This is your swagger license.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="licenseUrl"
+                render={({ field }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel>License URL</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Please insert your OpenAPI Url"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      This is your swagger license url.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
+        </div>
+        <Button className="mt-6" type="submit">
+          Submit
+        </Button>
       </form>
     </Form>
-  )
+  );
 }
