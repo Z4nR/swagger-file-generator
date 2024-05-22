6b2a508a720947c20ed1fbc65ed968a15296afc7
Z4nR <ari.comunity1@gmail.com>
2024-05-22 20:12:37 +0700

    chore: update json builder

--- src/utils/json.builder.ts
+++ src/utils/json.builder.ts
@@ -1,29 +1,36 @@
 import { CombinedState } from './state/types';
 
-const openAPI = (value: CombinedState) => {
-  return {
-    openapi: value.swagger,
-    info: {
-      title: value.title,
-      description: value.description,
-      license: value.license,
-      version: value.version,
-    },
-    tags: value.tags,
-  };
-};
+export const openAPI = (value: CombinedState) => {
+  const paths = value.paths.map((item) => {
+    const method = item.method.map((http) => {
+      return {
+        [http]: {
+          parameters: item.parameters,
+        },
+      };
+    });
+    return {
+      [item.endpoint]: {
+        ...method,
+      },
+    };
+  });
+  console.log(paths);
 
-const swaggerAPI = (value: CombinedState) => {
-  return {
-    swagger: value.swagger,
-    info: {
-      title: value.title,
-      description: value.description,
-      license: value.license,
-      version: value.version,
-    },
-    tags: value.tags,
-  };
+  if (value.title !== '2.0.0') {
+    return {
+      openapi: value.swagger,
+      info: {
+        title: value.title,
+        description: value.description,
+        license: value.license,
+        version: value.version,
+        [value.title]: 'Lorem',
+      },
+      tags: value.tags,
+      paths: paths,
+    };
+  } else {
+    console.log('Coming Soon');
+  }
 };
-
-export { openAPI, swaggerAPI };
