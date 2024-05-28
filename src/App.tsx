a09ef4002757a6cf753dc8ad9388d68ee802a6d1
Z4nR <ari.comunity1@gmail.com>
2024-05-28 08:34:13 +0700

    fix: change create by name to organization name

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,9 @@ const App: React.FC = () => {
       <h3 className="mt-6 text-2xl font-semibold text-center">
         Generate Your Swagger Spec
       </h3>
-      <h4 className="mt-2 text-sm font-semibold text-center">Create by ZAN</h4>
+      <h4 className="mt-2 text-sm font-semibold text-center">
+        Create by WhatNext
+      </h4>
       <Separator className="my-4" />
       <div className="flex gap-3">
         <div className="flex-1 p-4">
