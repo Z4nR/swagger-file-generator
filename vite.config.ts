4b84bbfbcba65635271ceb0a4b88232fb5a251a4
Z4nR <ari.comunity1@gmail.com>
2024-06-09 10:55:34 +0700

    chore: update setup and separate manual chunks

--- vite.config.ts
+++ vite.config.ts
@@ -60,4 +60,24 @@ export default defineConfig({
       },
     }),
   ],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
