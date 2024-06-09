3b3330eb7ed1429f50bd88aa40a164c00ed65c3c
Z4nR <ari.comunity1@gmail.com>
2024-06-09 10:47:51 +0700

    chore: setup for deploy in gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,63 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/swagger-file-generator',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        theme_color: '#ffffff',
+        background_color: '#008080',
+        display: 'standalone',
+        scope: '/swagger-file-generator',
+        start_url: '/swagger-file-generator',
+        description:
+          'SwagGen is website to simplify the process when creating swagger file',
+        name: 'SwagGen: Swagger File Generator',
+        short_name: 'SwagGen',
+        icons: [
+          {
+            src: '/WhatColors/pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: '/WhatColors/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/WhatColors/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/WhatColors/maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+      injectRegister: 'auto',
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{ts,css,html,ico,png,svg}'],
+        clientsClaim: true,
+        skipWaiting: true,
+        sourcemap: false,
+      },
+    }),
+  ],
 });
